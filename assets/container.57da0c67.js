6211bc427c9904884ef98f922e67a8acd349fc33
junhao.kuang <linker-junhao@outlook.com>
2021-12-25 18:35:43 +0800

    feat: done

---
+++ assets/container.57da0c67.js
@@ -0,0 +1,1 @@
+import{d as n,b as e,R as r}from"./vendor.ed1587f2.js";var o=n({name:"Container",setup(){return()=>e("div",{class:"h-screen"},[e(r,null,null)])}});export{o as default};
--- assets/index.cb0b92b1.js
+++
@@ -1,1 +0,0 @@
-import{d as n,b as e,R as r}from"./vendor.a41f8111.js";var o=n({name:"Work",setup(){return()=>e("div",null,[e(r,null,null)])}});export{o as default};
